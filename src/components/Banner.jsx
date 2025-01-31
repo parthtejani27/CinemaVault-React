6b3e70f43bcadcd994366b0b41581ac3fbe7026b
parthtejani27 <parthtejani27@gmail.com>
2025-01-30 19:52:42 -0500

    image path fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import backgroundImage from "./../assets/1.jpg";
 
 const Banner = () => {
   return (
     <div
       className="h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end"
-      style={{ backgroundImage: `url("src/assets/1.jpg")` }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="text-white text-2xl w-full text-center bg-black-900/60 p-4">
         Kraven The Hunter
